cefff32073253966dc8c6ca5368e2616641f93da
nickus83 <nickus.wanderer@gmail.com>
2016-04-07 17:00:08 +0300

    Working copy
    Has background, random grass, wolf and sheep. Player can move them. When
    a wolf sprite intercepts sheep spite, sheep vanishes.

---
+++ main.py
@@ -0,0 +1,228 @@
+# -*- coding: utf-8 -*-
+import os
+import os.path as osp
+import sys
+import random
+import pygame
+from pygame.locals import *
+from utils import *
+
+if not pygame.font:
+    print "Warning! Fonts disabled"
+if not pygame.mixer:
+    print "Warning! Sounds disabled"
+
+
+class Animal(pygame.sprite.Sprite):
+    """Basic animal that can move."""
+
+    def __init__(self, animal_images):
+        super(Animal, self).__init__()
+
+        self.image_down = [animal_images[str(self.animal_name) + '_down0'],
+                           animal_images[str(self.animal_name) + '_down1'],
+                           animal_images[str(self.animal_name) + '_down2']]
+        self.iter_down = 0
+
+        self.image_left = [animal_images[str(self.animal_name) + '_left0'],
+                           animal_images[str(self.animal_name) + '_left1'],
+                           animal_images[str(self.animal_name) + '_left2']]
+        self.iter_left = 0
+
+        self.image_right = [animal_images[str(self.animal_name) + '_right0'],
+                            animal_images[str(self.animal_name) + '_right1'],
+                            animal_images[str(self.animal_name) + '_right2']]
+        self.iter_right = 0
+
+        self.image_up = [animal_images[str(self.animal_name) + '_up0'],
+                         animal_images[str(self.animal_name) + '_up1'],
+                         animal_images[str(self.animal_name) + '_up2']]
+        self.iter_up = 0
+
+        self.image = self.image_down[0]  # init image
+        self.rect = self.image.get_rect()
+
+
+    def move_down(self):
+        self.rect.move_ip(0, 5)
+
+        self.iter_down += 1
+        if self.iter_down >= len(self.image_down):
+            self.iter_down = 0
+
+        self.image = self.image_down[self.iter_down]
+
+    def move_left(self):
+        self.rect.move_ip(-5, 0)
+
+        self.iter_left += 1
+        if self.iter_left >= len(self.image_left):
+            self.iter_left = 0
+
+        self.image = self.image_left[self.iter_left]
+
+    def move_right(self):
+        self.rect.move_ip(5, 0)
+
+        self.iter_right += 1
+        if self.iter_right >= len(self.image_right):
+            self.iter_right = 0
+
+        self.image = self.image_right[self.iter_right]
+
+    def move_up(self):
+        self.rect.move_ip(0, -5)
+
+        self.iter_up += 1
+        if self.iter_up >= len(self.image_up):
+            self.iter_up = 0
+
+        self.image = self.image_up[self.iter_up]
+
+
+class Sheep(Animal):
+    """Sheep animal, controlled with direction keys."""
+
+    def __init__(self, animal_images):
+        self.animal_name = self.__class__.__name__
+        super(Sheep, self).__init__(animal_images)
+        self.rect.center = (width / 2, height / 2)
+
+    def update(self, pressed_keys):
+
+        if pressed_keys[K_DOWN]:
+            self.move_down()
+        elif pressed_keys[K_LEFT]:
+            self.move_left()
+        elif pressed_keys[K_RIGHT]:
+            self.move_right()
+        elif pressed_keys[K_UP]:
+            self.move_up()
+
+        if self.rect.left < 0:
+            self.rect.left = 0
+        elif self.rect.right > width:
+            self.rect.right = width
+        if self.rect.top <= 0:
+            self.rect.top = 0
+        elif self.rect.bottom >= height:
+            self.rect.bottom = height
+
+
+class Wolf(Animal):
+    """Sheep animal, controlled with direction keys."""
+
+    def __init__(self, animal_images):
+        self.animal_name = self.__class__.__name__
+        super(Wolf, self).__init__(animal_images)
+
+    def update(self, pressed_keys):
+
+        if pressed_keys[K_s]:
+            self.move_down()
+        elif pressed_keys[K_a]:
+            self.move_left()
+        elif pressed_keys[K_d]:
+            self.move_right()
+        elif pressed_keys[K_w]:
+            self.move_up()
+
+        if self.rect.left < 0:
+            self.rect.left = 0
+        elif self.rect.right > width:
+            self.rect.right = width
+        if self.rect.top <= 0:
+            self.rect.top = 0
+        elif self.rect.bottom >= height:
+            self.rect.bottom = height
+       
+
+class Item(pygame.sprite.Sprite):
+    """Items on the ground."""
+
+    def __init__(self):
+        super(Item, self).__init__()
+
+        self.image = env_images[random.choice(env_images.keys())]
+        self.rect = self.image.get_rect()
+        self.rect.center = (random.randrange(width), random.randrange(height))
+
+
+class Grass(pygame.sprite.Sprite):
+    """Grass sprie"""
+
+    def __init__(self):
+        super(Grass, self).__init__()
+
+        self.image = grass_images[random.choice(grass_images.keys())]
+        self.rect = self.image.get_rect()
+        self.rect.center = (random.randrange(width), random.randrange(height))
+
+
+if __name__ == '__main__':
+
+    # size = width, height = 1280, 800
+    size = width, height = 1920, 1080
+    FPS = 20
+    done = False
+    clock = pygame.time.Clock()
+
+    pygame.init()
+    display_mode = pygame.FULLSCREEN | pygame.HWSURFACE
+    # display_mode = 0
+    screen = pygame.display.set_mode(size, display_mode)
+
+    env_images, grass_images, animal_images = import_images()
+
+    background = pygame.Surface(screen.get_size())
+    for x in range(width / 32):
+        for y in range(height / 32):
+            background.blit(env_images['background_tile'], (x * 32, y * 32))
+
+    animals = pygame.sprite.Group()
+    items = pygame.sprite.Group()
+    grass = pygame.sprite.Group()
+
+    sheep = Sheep(animal_images)
+    animals.add(sheep)
+
+    wolf = Wolf(animal_images)
+    animals.add(wolf)
+
+    new_item = Item()
+    items.add(new_item)
+
+    for i in range(40):
+        new_grass = Grass()
+        grass.add(new_grass)
+
+    while not done:
+        for event in pygame.event.get():
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    done = True
+                elif event.key == K_p:
+                    new_item = Item(env_images)
+                    items.add(new_item)
+            elif event.type == pygame.QUIT:
+                done = True
+        
+        pressed_keys = pygame.key.get_pressed()
+        sheep.update(pressed_keys)
+        wolf.update(pressed_keys)
+
+        if pygame.sprite.collide_mask(sheep, wolf):
+            sheep.kill()
+
+        screen.blit(background, (0, 0))  # first background update
+        for entity in grass:
+            screen.blit(entity.image, entity.rect)
+        for entity in items:
+            screen.blit(entity.image, entity.rect)  # second items update
+        for entity in animals:
+            screen.blit(entity.image, entity.rect)  # tird animals update
+      
+        pygame.display.update()
+        clock.tick(FPS)
+
+    pygame.quit()
---
+++ utils.py
@@ -0,0 +1,165 @@
+# -*- coding: utf-8 -*-
+import os
+import os.path as osp
+import math
+import pygame
+from pygame.locals import *
+
+
+def load_image(name, colorkey=None):
+
+    fullname = osp.join('data', 'images', name)
+
+    try:
+        image = pygame.image.load(fullname)
+    except pygame.error, message:
+        print 'Cannot load image:', name
+        raise SystemExit, message
+
+    image = image.convert()
+
+    if colorkey is not None:
+        if colorkey is -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey, RLEACCEL)
+
+    return image
+
+
+def load_sound(name):
+
+    class NoneSound:
+
+        def play(self): pass
+
+    if not pygame.mixer:
+        return NoneSound()
+
+    fullname = osp.join('data', name)
+
+    try:
+        sound = pygame.mixer.Sound(fullname)
+    except pygame.error, message:
+        print 'Cannot load sound:', wav
+        raise SystemExit, message
+
+    return sound
+
+
+def load_png(name, colorkey=None):
+
+    fullname = osp.join('data', 'images', name)
+
+    try:
+        image = pygame.image.load(fullname)
+    except pygame.error, message:
+        print 'Cannot load image:', name
+        raise SystemExit, message
+
+    image = image.convert_alpha()
+
+    if colorkey is not None:
+        if colorkey is -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey, RLEACCEL)
+
+    return image
+
+
+def import_images():
+    forest_source = load_png("forest.png")
+    animals_source = load_png("animals1.png")
+    wolf_source = load_png("wolf.png")
+
+    return [dict(background_tile=forest_source.subsurface(0, 0, 32, 32),
+                 hubble2=forest_source.subsurface(32, 0, 32, 32),
+                 hubble1=forest_source.subsurface(64, 0, 32, 32),
+                 hubble5=forest_source.subsurface(96, 0, 32, 32),
+                 stump1=forest_source.subsurface(128, 0, 32, 32),
+                 rocks3=forest_source.subsurface(160, 0, 32, 32),
+                 bush_flower1=forest_source.subsurface(192, 0, 32, 32),
+                 bush_flower2=forest_source.subsurface(224, 0, 32, 32),
+
+                 hubble3=forest_source.subsurface(32, 32, 32, 32),
+                 hubble_flower=forest_source.subsurface(64, 32, 32, 32),
+                 hubble6=forest_source.subsurface(96, 32, 32, 32),
+                 stump2=forest_source.subsurface(128, 32, 32, 32),
+                 rocks2=forest_source.subsurface(160, 32, 32, 32),
+                 bush1=forest_source.subsurface(192, 32, 32, 32),
+                 bush2=forest_source.subsurface(224, 32, 32, 32),
+
+                 nenuphar1=forest_source.subsurface(0, 64, 32, 32),
+                 nenuphar2=forest_source.subsurface(32, 64, 32, 32),
+                 sprout1=forest_source.subsurface(64, 64, 32, 32),
+                 sprout2=forest_source.subsurface(96, 64, 32, 32),
+                 mushroums=forest_source.subsurface(128, 64, 32, 32),
+                 rocks1=forest_source.subsurface(160, 64, 32, 32),
+                 wood1=forest_source.subsurface(192, 64, 32, 32),
+                 wood2=forest_source.subsurface(224, 64, 32, 32),
+
+                 ),
+            dict(grass1=forest_source.subsurface(96, 96, 32, 32),
+                 grass3=forest_source.subsurface(96, 128, 64, 64),
+                 grass2=forest_source.subsurface(128, 96, 32, 32),
+                 grass4=forest_source.subsurface(0, 96, 96, 96),
+                 ),
+            dict(
+                pig_down1=animals_source.subsurface(0, 0, 48, 48),
+                pig_down0=animals_source.subsurface(48, 0, 48, 48),
+                pig_down2=animals_source.subsurface(96, 0, 48, 48),
+                pig_left1=animals_source.subsurface(0, 48, 48, 48),
+                pig_left0=animals_source.subsurface(48, 48, 48, 48),
+                pig_left2=animals_source.subsurface(96, 48, 48, 48),
+                pig_right1=animals_source.subsurface(0, 96, 48, 48),
+                pig_right0=animals_source.subsurface(48, 96, 48, 48),
+                pig_right2=animals_source.subsurface(96, 96, 48, 48),
+                pig_up1=animals_source.subsurface(0, 144, 48, 48),
+                pig_up0=animals_source.subsurface(48, 144, 48, 48),
+                pig_up2=animals_source.subsurface(96, 144, 48, 48),
+
+                goat_down1=animals_source.subsurface(144, 0, 48, 48),
+                goat_down0=animals_source.subsurface(192, 0, 48, 48),
+                goat_down2=animals_source.subsurface(240, 0, 48, 48),
+                goat_right1=animals_source.subsurface(144, 48, 48, 48),
+                goat_right0=animals_source.subsurface(192, 48, 48, 48),
+                goat_right2=animals_source.subsurface(240, 48, 48, 48),
+                goat_left1=animals_source.subsurface(144, 96, 48, 48),
+                goat_left0=animals_source.subsurface(192, 96, 48, 48),
+                goat_left2=animals_source.subsurface(240, 96, 48, 48),
+                goat_up1=animals_source.subsurface(144, 144, 48, 48),
+                goat_up0=animals_source.subsurface(192, 144, 48, 48),
+                goat_up2=animals_source.subsurface(240, 144, 48, 48),
+
+                Sheep_down1=animals_source.subsurface(288, 0, 48, 48),
+                Sheep_down0=animals_source.subsurface(336, 0, 48, 48),
+                Sheep_down2=animals_source.subsurface(384, 0, 48, 48),
+                Sheep_left1=animals_source.subsurface(288, 48, 48, 48),
+                Sheep_left0=animals_source.subsurface(336, 48, 48, 48),
+                Sheep_left2=animals_source.subsurface(384, 48, 48, 48),
+                Sheep_right1=animals_source.subsurface(288, 96, 48, 48),
+                Sheep_right0=animals_source.subsurface(336, 96, 48, 48),
+                Sheep_right2=animals_source.subsurface(384, 96, 48, 48),
+                Sheep_up1=animals_source.subsurface(288, 144, 48, 48),
+                Sheep_up0=animals_source.subsurface(336, 144, 48, 48),
+                Sheep_up2=animals_source.subsurface(384, 144, 48, 48),
+
+                Wolf_down1=wolf_source.subsurface(0, 0, 64, 64),
+                Wolf_down0=wolf_source.subsurface(64, 0, 64, 64),
+                Wolf_down2=wolf_source.subsurface(128, 0, 64, 64),
+                Wolf_left1=wolf_source.subsurface(0, 64, 64, 32),
+                Wolf_left0=wolf_source.subsurface(64, 64, 64, 32),
+                Wolf_left2=wolf_source.subsurface(128, 64, 64, 32),
+                Wolf_right1=wolf_source.subsurface(0, 114, 64, 32),
+                Wolf_right0=wolf_source.subsurface(64, 114, 64, 32),
+                Wolf_right2=wolf_source.subsurface(128, 114, 64, 32),
+                Wolf_up1=wolf_source.subsurface(0, 154, 64, 38),
+                Wolf_up0=wolf_source.subsurface(64, 154, 64, 38),
+                Wolf_up2=wolf_source.subsurface(128, 154, 64, 38),
+    )]
+
+def round_32(x, base=32):
+    """Round x to base. Default 32."""
+    if x == 0:
+        return 0
+    else:
+        return int(math.ceil(float(x) / base)) - 1
